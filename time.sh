bdcb94b2d3ff5c45fc6c3fa8dbea634ee7874752
Sam Burdick <smburdick@pugetsound.edu>
2018-06-22 19:35:55 -0700

    deleted old query files

--- time.sh
+++
@@ -1,4 +0,0 @@
-for ((i=0; i<=1200; i+=10))
-do
-    /usr/bin/time -ao time.txt -f "$i\t%E" ./master "$i"
-done
--- time_cent.sh
+++
@@ -1,4 +0,0 @@
-for ((i=0; i<=1200; i+=10))
-do
-    /usr/bin/time -ao time_cent.txt -f "$i\t%E" ./master_cent "$i"
-done
